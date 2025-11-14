14f9da619160c93cbe9b4b508cf2158484df6a18
Noro <norbert.bodnar@cleevio.com>
2025-11-14 22:08:38 +0100

    Improved error handling

--- backend/src/main/kotlin/cz/bodnor/serviceslicer/infrastructure/exception/GlobalExceptionHandler.kt
+++ backend/src/main/kotlin/cz/bodnor/serviceslicer/infrastructure/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,8 @@
 package cz.bodnor.serviceslicer.infrastructure.exception
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,7 +11,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
+    private val logger = KotlinLogging.logger {}
+
     @ExceptionHandler(ApplicationError::class)
-    fun handleAppErrors(exception: RuntimeException): ResponseEntity<Any> =
-        ResponseEntity.badRequest().body(exception.message)
+    fun handleAppErrors(exception: RuntimeException): ResponseEntity<Any> {
+        logger.error(exception) { "Application error: ${exception.message}" }
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.message)
+        return ResponseEntity.badRequest().body(problemDetail)
+    }
 }
